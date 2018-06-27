c5851ef9c971b0401cc80154d3f1abb13b6ab0eb
Reid Mitchell <reidmitchell1@gmail.com>
2018-06-27 08:12:56 -0700

    Fix commonjs tests

--- src/transforms/tokens-to-commonjs.js
+++ src/transforms/tokens-to-commonjs.js
@@ -8,12 +8,12 @@ const {
 module.exports = withValidConfig(config => {
   const { settings: { namespace } = {} } = config;
   return (
-    'module.exports = [\n' +
+    'module.exports = {\n' +
     mapTokens(config, ({ tokenType, token: { id, value, description } }) => {
       const varName = camelCaseVariableName({ tokenType, namespace, id });
       const comment = !description ? '' : toDoubleSlashComment(description, 2) + '\n';
       return comment + `  ${varName}: '${value}'`;
     }).join(',\n\n') +
-    '\n];'
+    '\n};'
   );
 });
