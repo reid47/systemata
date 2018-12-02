93ea84ae03213b120c1b8d08bbf1b3b20a43561f
Reid Mitchell <reidmitchell1@gmail.com>
2018-12-02 02:05:17 -0800

    Refactor getConfig to try multiple files

--- src/cli/get-config.ts
+++ src/cli/get-config.ts
@@ -4,16 +4,31 @@ import { error } from './error';
 
 export function getConfig(input: CliInput): Config | undefined {
   const { configFile } = input.args;
+  const cwd = process.cwd();
 
-  if (!configFile || typeof configFile !== 'string') {
-    error("No config file specified. Use '-c' or '--config' to pass a path to a system config file.");
+  const possibleConfigFiles: string[] = [
+    path.resolve(cwd, 'systematic.config.js'),
+    path.resolve(cwd, 'systematic.config.json')
+  ];
+
+  if (typeof configFile === 'string') {
+    possibleConfigFiles.unshift(path.resolve(cwd, configFile));
   }
 
-  let config: Config;
-  try {
-    config = require(path.resolve(process.cwd(), configFile as string));
-  } catch (err) {
-    error(`Error loading config file: ${err.message}`);
+  let config: Config | undefined;
+  const errorMessages = [];
+
+  for (let i = 0; i < possibleConfigFiles.length; i++) {
+    try {
+      config = require(possibleConfigFiles[i]);
+      break;
+    } catch (err) {
+      errorMessages.push(err.message);
+    }
+  }
+
+  if (!config) {
+    error(`Could not load config file: ${errorMessages.join(' ')}`);
     return;
   }
 
